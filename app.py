9f038401bd9709adb258467cb867761ac4915f9c
Rohan Varma <rvarm1@ucla.edu>
2016-08-03 09:41:54 -0500

    Add index route

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
 def hello_world():
     print "change made"
     return render_template('index.html')
