6c74262d11ef3fd9ca966099e50c80e2d96ed08a
Rohan Varma <rvarm1@ucla.edu>
2016-08-04 12:58:27 -0500

    Add planning comments

--- main.py
+++ main.py
@@ -5,9 +5,19 @@ from db_interactor import DBInteractor
 from preprocess import Preprocessor
 
 if __name__ == '__main__':
+    #create a DB interactor
     interactor = DBInteractor("batting")
     df = interactor.get_current_data_frame()
     arr = interactor.df_to_numpy_matrix()
+    #don't forget to disconnect
     interactor.disconnect()
+    #create a preprocessor to preprocess the data
     processor = Preprocessor(arr)
-    print processor.get_shape()
+    #print processor.get_shape()
+    #after preprocessing the data, cluster it.
+
+
+
+    #build a locality sensitive hash table
+
+    
