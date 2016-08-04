801cb6344dc59955a8f74d4d407f62ea9d22d52a
Rohan Varma <rvarm1@ucla.edu>
2016-08-04 14:28:26 -0500

    Add preprocessing stuff

--- main.py
+++ main.py
@@ -6,14 +6,15 @@ from preprocess import Preprocessor
 
 if __name__ == '__main__':
     #create a DB interactor
-    interactor = DBInteractor("batting")
+    interactor = DBInteractor("season_batting")
     df = interactor.get_current_data_frame()
     arr = interactor.df_to_numpy_matrix()
     #don't forget to disconnect
     interactor.disconnect()
     #create a preprocessor to preprocess the data
-    processor = Preprocessor(arr)
-    processor.print_curr_arr()
+    processor = Preprocessor(arr, df)
+    #processor.print_curr_arr()
+    print processor.get_shape()
     #print processor.get_shape()
     #after preprocessing the data, cluster it.
 
