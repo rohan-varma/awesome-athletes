3e2cc39ade3ba2e6a483d8d1d416ea1c7346067c
Rohan Varma <rvarm1@ucla.edu>
2016-08-05 15:31:31 -0500

    HACKATHONS!!!!

---
+++ test.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    q = "similar to Barry Zito in 2004"
+    li = q.split()
+    name = li[2]
+    last = li[3]
+    year = int(li[5])
+    print name
+    print last
+    print year
