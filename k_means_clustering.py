7cecdb3f2f56a3efe3d3a853fc817e2560a6fe13
Alec Knutsen <aqknutsen@gmail.com>
2016-08-05 09:08:33 -0500

    Finish k-means

--- k_means_clustering.py
+++ k_means_clustering.py
@@ -3,8 +3,6 @@ import random
 import numpy as np
 import math
 
-
-
 class kmeans:
     def __init__(self):
         pass
@@ -18,25 +16,22 @@ class kmeans:
         for x in range(0, np.shape(means_matrix)[0]):
 
             rand_index = random.randint(0, (len(rand_numbers)-1))
-            rand_vec = data_matrix[rand_numbers[rand_index], :].reshape(np.shape(data_matrix)[1])
-            rand_numbers = rand_numbers[:rand_index] + rand_numbers[rand_index+1 :]
-            means_matrix[x:,] = rand_vec
+            means_matrix[x:,] = data_matrix[rand_numbers[rand_index], :].reshape(np.shape(data_matrix)[1])
+            rand_numbers = rand_numbers[:rand_index] + rand_numbers[rand_index + 1:]
 
         return means_matrix
 
     def k_means_algorithm(self, data, means):
 
-        previous_cluster_num = []
+        previous_cluster_num = [0] * np.shape(data)[0]
 
-        print(means)
+        cluster_num = [0] * np.shape(data)[0]
 
         while(True):
 
-            cluster_num = []
-
             for i in range(0, np.shape(data)[0]):
 
-                sum_of_squares_list = []
+                sum_of_squares_list = [0] * np.shape(means)[0]
 
                 for j in range(0, np.shape(means)[0]):
 
@@ -46,53 +41,55 @@ class kmeans:
 
                         sum = sum + (data[i, k] - means[j, k]) * (data[i, k] - means[j, k])
 
-                    sum_of_squares_list.append(math.sqrt(sum))
+                    sum_of_squares_list.insert(j, math.sqrt(sum))
+                    sum_of_squares_list.pop(j + 1)
 
+                cluster_num.insert(i,sum_of_squares_list.index(min(sum_of_squares_list)))
+                cluster_num.pop(i+1)
 
-                cluster_num.append(sum_of_squares_list.index(min(sum_of_squares_list)))
-
-            if (cmp(cluster_num, previous_cluster_num)):
+            print(cluster_num)
+            if (self.comparelists(cluster_num, previous_cluster_num)):
 
                 return cluster_num
 
                 break
 
             else:
+
                 previous_cluster_num = cluster_num
 
-                var = 0
+                for l in range(0, np.shape(means)[0]):
 
-                while var < np.shape(means)[0]:
-
-                    common_cluster_data_points = np.zeros((1, np.shape(data)[1]))
+                    rows_in_which_clusters = np.zeros((1, np.shape(data)[1]))
                     num_points_in_cluster = 0
 
-                    for l in range(0, len(cluster_num)):
+                    for m in range(0, len(cluster_num)):
 
-                        if cluster_num[l]==var:
-                            common_cluster_data_points += data[l, :]
+                        if cluster_num[m]==l:
+
+                            rows_in_which_clusters += data[m, :]
                             num_points_in_cluster = num_points_in_cluster + 1
 
-                    common_cluster_data_points = common_cluster_data_points/num_points_in_cluster
+                    rows_in_which_clusters = rows_in_which_clusters/num_points_in_cluster
+                    means[l, :] = rows_in_which_clusters[0, :]
 
-                    means[var, :] = common_cluster_data_points[1, :]
+    def comparelists(self, list1, list2):
 
+        for i in range(0, len(list1)):
 
+            if list1[i] == list2[i]:
 
+                continue
+            else:
 
+                return False
 
-
-
-
-
-
-
-
-
+        return True
 
 if __name__ == '__main__':
     k=kmeans()
-    matrix = np.matrix('1 2; 3 4; 5 6; 7 8; 9 10; 11 12; 13 14; 15 16; 17 18; 19 20')
+    matrix = np.random.randint(5, size=(30, 20))
     print(matrix)
-    means= k.initialize_means(matrix, 2)
-    print(k.k_means_algorithm(matrix,means))
+    means= k.initialize_means(matrix, 4)
+    final = k.k_means_algorithm(matrix,means)
+    print(final)
